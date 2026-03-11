7a90fe3d6b5f52ffe75cc254fccdba10c41a1634
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2026-03-11 16:29:20 -0300

    FIX: Update nested replies routes to include topic slug and ID in a proper scoping block (#18)
    * FIX: Update nested replies routes to include topic slug and ID in a proper scoping block
    
    Without this change, having this plugin installed would cause scope mapping errors when creating api keys.
    
    More context can be seen in /t/-/179529/9
    
    * DEV: add test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 DiscourseNestedReplies::Engine.routes.draw do
-  get "/" => "nested_topics#respond"
-  get "/roots" => "nested_topics#roots"
-  get "/children/:post_number" => "nested_topics#children"
-  get "/context/:post_number" => "nested_topics#context"
-  put "/pin" => "nested_topics#pin"
+  scope "/nested/:slug/:topic_id", constraints: { topic_id: /\d+/ } do
+    get "/" => "nested_topics#respond"
+    get "/roots" => "nested_topics#roots"
+    get "/children/:post_number" => "nested_topics#children"
+    get "/context/:post_number" => "nested_topics#context"
+    put "/pin" => "nested_topics#pin"
+  end
 end
 
-Discourse::Application.routes.draw do
-  mount ::DiscourseNestedReplies::Engine, at: "/nested/:slug/:topic_id"
-end
+Discourse::Application.routes.draw { mount ::DiscourseNestedReplies::Engine, at: "/" }
